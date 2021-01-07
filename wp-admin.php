d82882d58802bf4bb3a254c3e49b3615e27329e3
Viktor Novikov <thesuperpothead@gmail.com>
2021-01-07 13:50:46 +0200

    Update wp-admin.php

--- wp-admin.php
+++ wp-admin.php
@@ -17,3 +17,19 @@ add_filter("manage_pa_colour_custom_column", function ( $string, $column_name, $
     }
     return $string;
 }, 10, 3);
+
+/*
+ * Add notices on post updated
+ */
+add_action('save_post_events', 'post_event_to_lcrig', 20, 3);
+function post_event_to_lcrig( $post_ID, $post, $update ) {
+    add_filter( 'redirect_post_location', 'redirect_post_location_lcrig_posted', 90, 2 );
+}
+function redirect_post_location_lcrig_posted($location, $post_id) {
+    return add_query_arg( [ 'use' => 'updated' ], $location );
+}
+add_action( 'admin_notices', 'admin_notices_event_lcrig' );
+function admin_notices_event_lcrig() {
+    if (isset($_GET['use']) && in_array($_GET['use'], ['updated', 'created', 'failed']))
+        echo '<div id="message" class="notice notice-success is-dismissible"><p>This event was updated on LCRIG</p></div>';
+}
